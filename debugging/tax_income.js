c640d7faf24d3be7553d2d45f6a7348ea6df647d
Alisa Grigorieva <73264312+alisa-grigorieva@users.noreply.github.com>
2022-09-03 23:34:26 -0500

    Update tax_income.js

--- debugging/tax_income.js
+++ debugging/tax_income.js
@@ -8,39 +8,49 @@ var calculateIncome = function ()
     var floatAnnualIncome = parseInt($("annualIncome").value);
     var intUserAge = parseInt($("age").value);
     var floatTaxRate, floatAnnualTax;
-    if (intUserAge >= 65)
+    if (intUserAge < 0 || floatAnnualIncome < 0)
     {
+	alert("Please enter a positive number for your age and annual income.\nThank you!")
+	return false;
+     }
+     else if (intUserAge <=13)
+     {
+	alert ("The age of "+ intUserAge + " is below minimum age for employment in Oklahoma.");
+	return false;
+     }
+     else if (intUserAge >= 65)
+     {
     	if (floatAnnualIncome < 30000)
         {
     		floatTaxRate = .025;
     	}
     	else
         {
-    		if(floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
-            {
+		if(floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+            	{
     			floatTaxRate = .05;
     		}
     		else
-            {
+            	{
     			floatTaxRate = .20;
-    	    }
+    	    	}
         } 
-    }
-    else
-    {
+     }
+     else
+     {
     	if (floatAnnualIncome < 30000)
     	{
-    		floatTaxRate = .05;
-    	}
-   		else if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
-   		{
-   			floatTaxRate = .10;
-   		}
-   		else
-   		{
-   			floatTaxRate = .20;
-   		}
+    	floatTaxRate = .05;
+   	}
+   	else if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+	{
+   		floatTaxRate = .10;
 	}
+   	else
+	{
+   		floatTaxRate = .20;
+   	}
+      }
 
 	floatAnnualTax = floatAnnualIncome * floatTaxRate;
 
